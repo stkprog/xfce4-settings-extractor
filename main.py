b13bbf0678d4a35da4b59e8c582b053320fb441d
nun1o2that <nuggetindustry@gmail.com>
2024-11-14 14:07:42 +0100

    edit function comments to docstring standard

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+# A python script that grabs all XFCE4 settings using xfconf-query
+# and saves them into a .sh-file which can be used to restore
+# those exact configurations.
+# Copyright (C) 2024 St. K.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import os       # To use shell commands
 import typing   # Type hinting
 import sys      # To use command line arguments
 import re       # Regular expressions
 
-# Nice header output for each channel
 def get_channel_header(channel_name : str) -> str:
+    """Returns a nice header for the current channel in the output script."""
     header = "#\n"
     header += "#    Channel: " + channel_name + "\n"
     header += "#\n"
     return header
 
-# Get all available channels using the shell command xfconf-query
 def get_all_channels() -> list:
+    """Get all available channels using the shell command xfconf-query."""
     needed_channels = []
     if "-a" in sys.argv or "--all" in sys.argv:
         needed_channels = [
@@ -36,8 +55,8 @@ def get_all_channels() -> list:
 
     return new_result
 
-# Get all available properties for the specified channel
 def get_all_properties_of_channel(channel_name : str) -> list:
+    """Get all available properties for the specified channel"""
     # Get all properties in the form of a list
     result = os.popen("xfconf-query -c " + channel_name + " -l").read().splitlines()
 
@@ -50,12 +69,12 @@ def get_all_properties_of_channel(channel_name : str) -> list:
     result = filter(lambda x : not x.startswith("/plugins/") and not x == "/general/button_layout", result)
     return result
 
-# Insert the escape character at angle brackets so the resulting shell script will work
 def insert_escape_backslash_at_angle_brackets(to_be_converted : str) -> str:
+    """Insert the escape character at angle brackets so the resulting shell script will work."""
     return to_be_converted.replace("<", "\\<").replace(">", "\\>")
 
-# Process array values correctly
 def handle_array(property_value : str) -> list:
+    """Process a property holding an array of values, rather than a single value, correctly."""
     # Split the string into an array of lines
     value_array = property_value.splitlines()
     # Pop the first two because they're irrelevant
@@ -71,28 +90,33 @@ def handle_array(property_value : str) -> list:
 
     return value_array
 
-# Check if string is numeric
 def is_numeric(value : str) -> bool:
+    """Check if the given string is numeric."""
     # print(re.search("[+0-9],[+0-9]", value) is not None or value.strip().isdecimal())
     if re.search("[+0-9],[+0-9]", value) is not None or value.isdecimal():
         return True
     else:
         return False
 
-# Convert commas (,) to dots (.)
 def handle_numeric(value : str) -> bool:
+    """Convert commas (,) in the given string to dots (.)."""
     return value.replace(",", ".")
 
-# Returns a hex-value array based on an array of rgb-values
-# E.g. ["rgb(255,255,255)", "rgb(0, 0, 0)"] to ["#FFFFFF", "#000000"]
 def handle_rgb(rgb_array : list) -> list:
+    """
+    Returns a hex-value array based on an array of rgb-values.
+    E.g.: ["rgb(255,255,255)", "rgb(0, 0, 0)"] is converted to ["#FFFFFF", "#000000"].
+    """
     hex_array = []
     for value in rgb_array:
-        hex_array.append(convert_to_hex(value))
+        hex_array.append(rgb_to_hex(value))
     return hex_array
 
-# E.g. returns "#FF00FF" from "rgb(255,0,255)"
-def convert_to_hex(rgb_string : str) -> str:
+def rgb_to_hex(rgb_string : str) -> str:
+    """
+    Returns a hex-value based on an rgb-value.
+    E.g.: "#FF00FF" is converted to "rgb(255,0,255)".
+    """
     tuple = re.findall(r"[0-9]+", rgb_string)
     r = int(tuple[0])
     g = int(tuple[1])
